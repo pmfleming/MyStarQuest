fb6ceddb2d5a5c9721f1e9bfef194c48ed9b4789
pmfleming <paul_m_fleming@hotmail.com>
2026-02-25 12:13:16 +0100

    Add Capacitor Android project scaffold

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'com.mystarquest.app',
+  appName: 'MyStarQuest',
+  webDir: 'dist',
+}
+
+export default config
